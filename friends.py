e6b471fa4ba976c6e6703c1291f717b5d62da0cb
shruthinayak <rn.shruthi@gmail.com>
2016-02-09 18:35:42 -0600

    Modified code to show correct error message

--- friends.py
+++ friends.py
@@ -1,23 +1,29 @@
 '''
 USAGE:
-python friends.py "<path-to-the-friends-folder"'''
+python friends.py "<path-to-the-friends-folder>"'''
 
 import sys
 import random
 import os
 from os import listdir
 from os.path import isfile, isdir, join
-path = sys.argv[1]
-print path
-seasons = [ f for f in listdir(path) if isdir(join(path,f)) ]
-print seasons
-season_choice = random.randint(0,len(seasons))
-path+=str(seasons[season_choice])
-print path
-episodes = [ f for f in listdir(path) if isfile(join(path,f)) ]
-episode_choice = random.randint(0,len(episodes))
-video = (path+"/"+episodes[episode_choice]).replace(" ","\\ ").replace("'","\\'").replace(".","\.")
-print video
-cmd='vlc '+video
-os.system(cmd)
-
+if len(sys.argv)==2:
+	path = sys.argv[1]
+	print path
+	seasons = [ f for f in listdir(path) if isdir(join(path,f)) ]
+	print seasons
+	season_choice = random.randint(0,len(seasons))
+	path+=str(seasons[season_choice])
+	print path
+	episodes = [ f for f in listdir(path) if isfile(join(path,f)) ]
+	episode_choice = random.randint(0,len(episodes))
+	video = (path+"/"+episodes[episode_choice]).replace(" ","\\ ").replace("'","\\'").replace(".","\.")
+	print video
+	cmd='vlc '+video
+	os.system(cmd)
+else:
+	print
+	print "USAGE:"
+	print "python friends.py <path-to-the-friends-folder>"
+	print "REQUIRED: "
+	print "VLC Media Player"
